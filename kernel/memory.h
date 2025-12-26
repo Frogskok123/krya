8d0bc0141f9674b0dc81e966b99f61f08d6ac2bf
Frogskok123 <39944364+Frogskok123@users.noreply.github.com>
2025-12-27 00:02:04 +0500

    Update memory.h

--- kernel/memory.h
+++ kernel/memory.h
@@ -1,16 +1,11 @@
-#pragma once
+#ifndef MEMORY_H
+#define MEMORY_H
 
-#include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/mm_types.h>
-
-phys_addr_t translate_linear_address(struct mm_struct *mm, uintptr_t va);
-
-bool read_physical_address(phys_addr_t pa, void *buffer, size_t size);
-
-bool write_physical_address(phys_addr_t pa, void *buffer, size_t size);
+#include <linux/types.h>
+#include <linux/mm.h>
 
 bool read_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size);
-
 bool write_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size);
 bool resolve_kernel_symbols(void);
+
+#endif
