00e02f035eefd08313bd424514f579553e70f015
Frogskok123 <39944364+Frogskok123@users.noreply.github.com>
2025-12-27 03:47:08 +0500

    Update entry.c

--- kernel/entry.c
+++ kernel/entry.c
@@ -22,48 +22,40 @@ static long dispatch_ioctl(struct file *const file, unsigned int const cmd, unsi
 {
     static COPY_MEMORY cm;
     static MODULE_BASE mb;
-    static char key[0x100] = {0};
     static char name[0x100] = {0};
-    static bool is_verified = false;
 
-    if (cmd == OP_INIT_KEY && !is_verified) {
-        if (copy_from_user(key, (void __user *)arg, sizeof(key) - 1) != 0)
-            return -EFAULT;
-        is_verified = true;
-        return 0;
-    }
-
-    if (!is_verified)
-        return -EACCES;
+    // МЫ УДАЛИЛИ ПРОВЕРКУ is_verified И OP_INIT_KEY [file:81]
 
     switch (cmd) {
-    case OP_READ_MEM:
-        if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
-            return -EFAULT;
-        if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
-            return -EIO;
-        break;
+        case OP_READ_MEM:
+            if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
+                return -EFAULT;
+            if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
+                return -EIO;
+            break;
 
-    case OP_WRITE_MEM:
-        if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
-            return -EFAULT;
-        if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
-            return -EIO;
-        break;
+        case OP_WRITE_MEM:
+            if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
+                return -EFAULT;
+            if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
+                return -EIO;
+            break;
 
-    case OP_MODULE_BASE:
-        if (copy_from_user(&mb, (void __user *)arg, sizeof(mb)) != 0 || 
-            copy_from_user(name, (void __user *)mb.name, sizeof(name) - 1) != 0)
-            return -EFAULT;
-        
-        mb.base = get_module_base(mb.pid, name);
-        if (copy_to_user((void __user *)arg, &mb, sizeof(mb)) != 0)
-            return -EFAULT;
-        break;
+        case OP_MODULE_BASE:
+            if (copy_from_user(&mb, (void __user *)arg, sizeof(mb)) != 0 ||
+                copy_from_user(name, (void __user *)mb.name, sizeof(name) - 1) != 0)
+                return -EFAULT;
+            
+            mb.base = get_module_base(mb.pid, name);
+            
+            if (copy_to_user((void __user *)arg, &mb, sizeof(mb)) != 0)
+                return -EFAULT;
+            break;
 
-    default:
-        return -EINVAL;
+        default:
+            return -EINVAL;
     }
+
     return 0;
 }
 
